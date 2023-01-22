60526f11416fb4bf12eac7777b9c5088dbb1e9f9
Arai Shota <me@arsho.dev>
2023-01-22 14:48:15 +0900

    Add AWSUME_PROFILE variable to right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -6,12 +6,17 @@ function _git_user_email
   echo (command git config --get user.email | cut -d@ -f1)
 end
 
+function _awsume_profile
+  echo (if set -q AWSUME_PROFILE; echo $AWSUME_PROFILE; else; echo 'N/A'; end)
+end
+
 function fish_right_prompt
   set -l blue (set_color blue)
+  set -l green (set_color green)
   set -l normal (set_color normal)
 
   if [ (_is_git_repo) ]
-    set git_user_email $normal '<' $blue(_git_user_email) $normal '>'
-    echo -n -s $git_user_email
+    set info $normal 'Git: ' $blue(_git_user_email) $normal ' | AWS: ' $green(_awsume_profile)
+    echo -n -s $info
   end
 end
