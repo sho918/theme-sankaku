0ea4f9bcb0e1e56058a64bad7e4ade95f4a77927
Arai Shota <me@arsho.dev>
2025-01-31 17:07:14 +0900

    Change AWS label

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,26 +1,26 @@
 function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
 function fish_prompt
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l red (set_color red)
-  set -l normal (set_color normal)
+    set -l blue (set_color blue)
+    set -l green (set_color green)
+    set -l red (set_color red)
+    set -l normal (set_color normal)
 
-  if [ (_git_branch_name) ]
-    set git_info $green(_git_branch_name)
-    set git_info ":$git_info"
+    if [ (_git_branch_name) ]
+        set git_info $green(_git_branch_name)
+        set git_info ":$git_info"
 
-    if [ (_is_git_dirty) ]
-      set git_info $red(_git_branch_name)
-      set git_info ":$git_info*"
+        if [ (_is_git_dirty) ]
+            set git_info $red(_git_branch_name)
+            set git_info ":$git_info*"
+        end
     end
-  end
 
-  echo -n -s $blue(prompt_pwd) $git_info $normal ' ▹ '
+    echo -n -s $blue(prompt_pwd) $git_info $normal ' ▹ '
 end
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,16 +1,16 @@
 function _git_user_email
-  echo (command git config --get user.email | cut -d@ -f1)
+    echo (command git config --get user.email | cut -d@ -f1)
 end
 
 function _awsume_profile
-  echo (if set -q AWSUME_PROFILE; echo $AWSUME_PROFILE; else; echo 'N/A'; end)
+    echo (if set -q AWSUME_PROFILE; echo $AWSUME_PROFILE; else; echo '-'; end)
 end
 
 function fish_right_prompt
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l normal (set_color normal)
+    set -l blue (set_color blue)
+    set -l green (set_color green)
+    set -l normal (set_color normal)
 
-  set info $normal $blue(_git_user_email) $normal ' | ' $green(_awsume_profile)
-  echo -n -s $info
+    set info $normal $blue(_git_user_email) $normal ' | ' $green(_awsume_profile)
+    echo -n -s $info
 end
